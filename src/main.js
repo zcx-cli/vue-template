f14e7467a2e5fe4206a9a8a7f29f58dbec6fa08c
zhengchengxuan <534370078@qq.com>
2022-11-22 01:24:24 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from "vue-router";
 import router from "./router";
+import store from './store';
 import App from './App.vue'
 import './assets/tailwind.css'
 import VueLazyLoad from 'vue-lazyload'
@@ -15,5 +16,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
