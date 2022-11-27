a5f0d513bcf9bee9045551c9c856f9eabb083d96
zhengchengxuan <534370078@qq.com>
2022-11-27 23:04:54 +0800

    chore

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,8 @@
 export default {
   name: 'App',
   components: {
-    // Tailwind,
-    // PxToVw,
-    // LazyLoad,
-    // MovieClip
   },
   data() {
-    const imgArr = [];
-    for (let index = 0; index < 16; index += 1) {
-      imgArr.push(require(`@/assets/gif/${index}.png`));
-    }
-    return {
-      imgArr
-    }
   }
 }
 </script>
