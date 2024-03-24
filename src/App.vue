be244e3f081a6134cf43260bf302a37f9738bc64
zhengchengxuan <534370078@qq.com>
2024-03-25 00:20:12 +0800

    chore

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
+    <button @click="handleClick">打开文件夹</button>
+    <p>{{ codeText }}</p>
     <router-view></router-view>
+
   </div>
 </template>
 
@@ -9,10 +12,62 @@ export default {
   name: 'App',
   components: {
   },
-  data() {
-  }
+  data () {
+    return {
+      codeText: ''
+    }
+  },
+
+  created () {
+  },
+  methods: {
+    async processHandler (handle) {
+      if (handle.kind === 'file') {
+        return handle
+      }
+      handle.children = []
+      const iter = await handle.entries();//获得文件夹中的所有内容
+      //iter:异步迭代器
+      for await (const info of iter) {
+        var subHandle = await this.processHandler(info[1]);
+        handle.children.push(subHandle)
+      }
+      return handle
+    },
+    async handleClick () {
+      const fileList = []
+      const res = await window.showDirectoryPicker({});
+      const detailAction = async (obj) => {
+        if (obj.entries) {
+          const dirs = obj.entries();
+          for await (const entry of dirs) {
+            if (entry[1].entries) {
+              // 文件夹，递归处理
+              detailAction(entry[1]);
+            } else {
+              // 文件
+              fileList.push({
+                name: entry[0],
+                path: obj.name,
+                fileHandle: entry[1],
+              });
+            }
+          }
+        }
+      };
+      await detailAction(res);
+      console.log("--fileList--", fileList);
+      fileList.forEach(async (item) => {
+        const file = await item.fileHandle.getFile();
+        console.log(' file', file)
+        const test = await file.text();
+        this.codeText = test
+        console.log(' codeText', this.codeText)
+      })
+
+    }
+  },
 }
 </script>
 
-<style>
-</style>
+<style></style>
--- src/page/demoPage.vue
+++ src/page/demoPage.vue
@@ -31,12 +31,12 @@
 			// MovieClip
 		},
 		data () {
-			const imgArr = [];
-			for (let index = 0; index < 16; index += 1) {
-				imgArr.push(require(`@/assets/gif/${index}.png`));
-			}
+			// const imgArr = [];
+			// for (let index = 0; index < 16; index += 1) {
+			// 	imgArr.push(require(`@/assets/gif/${index}.png`));
+			// }
 			return {
-				imgArr
+				// imgArr
 			}
 		}
 	}
